9db0f02e6e7f1ee60c6a759baa8d059d35ed09c4
Ansar <mmatestsenseonics@gmail.com>
2023-10-03 15:34:35 +0530

    Path removed

--- SPMTest/SPMTest/Package.swift
+++ SPMTest/SPMTest/Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SPMTest",
-            dependencies: [],
-            path: "SPMTest/SPMTest"),
+            dependencies: []),
     ]
 )
